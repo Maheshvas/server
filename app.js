bc929bafdec69f59fd0bd7e5a0c08cb6d2df610b
Maheshvas <maheshvasa567@gmail.com>
2022-09-18 11:58:09 +0530

    server

--- app.js
+++ app.js
@@ -1,71 +1,43 @@
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-const Post=require('./model/post');
-const bodyParser=require('body-parser');
-const multer=require('multer');
-const cors=require('cors');
+const express = require('express');
+require('dotenv').config();
+const mongoose = require("mongoose");
+const userRouter = require('./routes/user');
+
+const User = require('./models/user');
+
+const app = express();
+mongoose.connect("mongodb+srv://mahesh12:mahesh12@cluster0.vk2eq.mongodb.net/?retryWrites=true&w=majority").then(
+       () => console.log("connected mongoose")
+    ).catch(err => console.log(err))
+
+
+// app.use((req, res, next) => {
+//   req.on('data', chunk => {
+//     const data = JSON.parse(chunk);
+//     req.body = data;
+//     next();
+//   });
+// });
+
+app.use(express.json());
+app.use(userRouter);
+
+// const test = async (email, password) => {
+//   const user = await User.findOne({ email: email });
+//   const result = await user.comparePassword(password);
+//   console.log(result);
+// };
 
 
 
+app.get('/test', (req, res) => {
+  res.send('Hello world');
+});
 
-const storage=multer.diskStorage({
-    destination: function (req,file,cb) {
-        cb(null, 'uploads')
-    },
-    filename: function (req,file,cb){
-        const fileNameArr=file.originalname.split('.');
-        cb(null,file.fieldname + '-'+Date.now()+'.'+fileNameArr[fileNameArr.length-1]);
-    }
-})
-const upload=multer({storage:storage})
+app.get('/', (req, res) => {
+  res.json({ success: true, message: 'Welcome to backend zone!' });
+});
 
-async function connectDB(){
-    try {
-        return await mongoose.connect('mongodb+srv://mahesh12:mahesh12@cluster0.vk2eq.mongodb.net/?retryWrites=true&w=majority');
-    } catch(e) {
-        console.log(e);
-        throw error('database failed to connect to mongodb');
-    }
-}
-
-async function main(){
-    await connectDB();
-    app.use('/uploads',express.static('uploads'));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
-    app.use(cors());
-
-    app.get('/',function(req,res){
-        res.send({message:'welcome to insta clone on this port 8000' });
-    })
-
-    app.get('/api/v1/posts',async function(req,res){
-        const posts=await Post.find();
-        res.json({
-            posts
-        })
-    })
-    app.post('/api/v1/posts', upload.single('image'), async (req,res)=>{
-        const post = new Post({
-            image: req.file.filename,
-            author:req.body.author,
-            location:req.body.location,
-            description:req.body.description
-        });
-        
-        try{
-            const savedPost= await post.save();
-            res.json(savedPost);
-        }catch(e){
-            res.json({msg:e}); 
-        }
-    
-    });
-
-    
-
-    app.listen(process.env.PORT || 8000,()=>console.log('server started on port 8000'));
-}
-
-main()
+app.listen(8000, () => {
+  console.log('port is listening');
+});
---
+++ helper/imageUpload.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+  cloud_name: 'dkndasxaj', 
+  api_key: '988587985294853', 
+  api_secret: '-YE-_oCH7-JaTtTt8u9jE9QR7IA' 
+});
+
+module.exports = cloudinary;
